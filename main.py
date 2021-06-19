875300a4288b040089fea58e09bda3091ab4493e
victor abdiel lux juracan <3031591380108@ingenieria.usac.edu.gt>
2021-06-19 13:52:06 -0600

    insertar columnas y filas

--- main.py
+++ main.py
@@ -4,18 +4,11 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 from matriz import matriz
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     funciones_matriz = matriz()
     x = int(input('Ingresar posicion x :'))
     y = int(input('Ingresar posicion y :'))
-    funciones_matriz.insertar_x(x)
-    funciones_matriz.insertar_y(y)
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    funciones_matriz.insertar_y_x(x,y,funciones_matriz)
+    funciones_matriz.buscar_y(y)
+    funciones_matriz.buscar_x(x)
--- matriz.py
+++ matriz.py
@@ -4,24 +4,32 @@ class matriz():
     def __init__(self):
         self.cabecera = cabezera(0, 0)
         self.cabezaNodo = None
-    def insertar_x(self, X):
+    def buscar_x(self, X):
         print('insertar x')
         node = self.cabecera
         while node is not None:
             print(node.fil_x)
             if(node.fil_x == X):
-                return node
+                print('')
+#                return node
             node = node.siguiente
-        return None
-    def insertar_y(self,y):
+#        return None
+    def buscar_y(self,y):
         print('insertar y')
         node = self.cabecera
         while node is not None:
             print(node.fil_y)
             if(node.fil_y == y):
-                return node
+                print('')
+#                return node
             node = node.siguiente
-        return None
+#        return None
 
-    def insertar_nodo_cont(self):
+    def insertar_y_x(self,nuevoX,nuevoY,cabeza):
+        node = self.cabecera
+        if self.cabecera.siguiente is None:
+            node.siguiente = cabezera(nuevoX,nuevoY)
+            print
+
+    def insertar_nodo_cont(self,posx,posy,nuevo):
         print("insertar Color")
