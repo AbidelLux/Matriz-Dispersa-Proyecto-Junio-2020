f3eba8c9c7fb4512982a00d02d3f04ed0d6dd58d
victor abdiel lux juracan <3031591380108@ingenieria.usac.edu.gt>
2021-06-21 23:54:06 -0600

    Conectar botones

--- main.py
+++ main.py
@@ -6,16 +6,28 @@ from matriz import matriz
 from matriz_cont import matriz_cont
 from nodo_matriz import nodo_matriz
 from ventana_ui import *
-class MainWindow(QtWidgets.QMainWindow):
-    pass
+class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
+    def __init__(self, *args, **kwargs):
+        QtWidgets.QMainWindow.__init__(self,*args,**kwargs)
+        self.setupUi(self)
+        #self.btn_fin_turno.clicked.connect(self.insertar_pieza(2,2))
+        self.btn_estado.setText("Presionar")
+    def insertar_pieza(self,x,y):
+        funcion_matriz = matriz()
+        funcion_matriz.ordenar_cab_fila(y)
+        funcion_matriz.ordenar_cab_columna(x)
+    def estado(self):
+        fun = matriz()
+        fun.buscar_y(2)
+        fun.buscar_x(2)
 if __name__ == '__main__':
     funciones_matriz = matriz()
     func_matriz_cont = matriz_cont()
-    x = int(input('Ingresar posicion x :'))
-    y = int(input('Ingresar posicion y :'))
+    #x = int(input('Ingresar posicion x :'))
+    #y = int(input('Ingresar posicion y :'))
     cont = input('Ingresar color: ')
-    funciones_matriz.ordenar_cab_fila(y)
-    funciones_matriz.ordenar_cab_columna(x)
+    #funciones_matriz.ordenar_cab_fila(y)
+    #funciones_matriz.ordenar_cab_columna(x)
     print('***************************')
 
     funciones_matriz.insertar_x_final(4)
@@ -39,8 +51,8 @@ if __name__ == '__main__':
     #func_matriz_cont.mostrar(x,funciones_matriz)
 
     #func_matriz_cont.mostra(y,funciones_matriz)
-    funciones_matriz.buscar_y(y)
-    funciones_matriz.buscar_x(x)
+    funciones_matriz.buscar_y(2)
+    funciones_matriz.buscar_x(2)
     #funciones_matriz.buscar_x(x)
     print("********************************")
     print("probar insertar antes de 2 un 1 en columnas")
